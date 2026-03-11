07b459e039ee041ca0b342075af49622521da5a3
Couponszone <info@couponszone.co.in>
2026-03-11 21:31:01 +0530

    Remove .env to prevent pushing secrets

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 // netlify/functions/api.js
 const serverless = require("serverless-http");
-const app = require("../../../backend/src/app"); // path to your existing Express app
+const app = require("../../src/app"); // path to your existing Express app
 
 module.exports.handler = serverless(app);
